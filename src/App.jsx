0740dce88207cc4b957961729750fbc157cff08b
MaulanaRoyyanTsubaisa <maulanaroyyan33@gmail.com>
2025-05-19 23:22:42 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./global.css";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Navbar from "../components/Navbar";
+import Features from "../components/Features";
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
