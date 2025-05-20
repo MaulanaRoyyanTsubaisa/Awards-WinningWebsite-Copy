6fbcc01261484caea67ee917fdccbef897d0d241
MaulanaRoyyanTsubaisa <maulanaroyyan33@gmail.com>
2025-05-20 22:58:42 +0700

    update

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,6 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
-import VidePrev from "./VidPrev";
 
 import AnimatedTitle from "./AnimatedTitle";
 
